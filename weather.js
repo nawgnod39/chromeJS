77849c1dfe9fc6a27a689ee22a39d1e6477e9195
takdongwan <dongwantak@gmail.com>
2021-01-03 01:18:05 +0900

    Getting the Weather part Two API

--- weather.js
+++ weather.js
@@ -1,46 +1,58 @@
+const weather = document.querySelector(".js-weather");
 const API_KEY ="7f73d4d1c949a39dc8eceb1c1b74c286";
 const COORDS ='coords';
 
 
-function saveCoords(coordsObj){
-    localStorage.setItem(COORDS,JSON.stringify(coordsObj));
-}
 
-function handleGeoSucces(position){
-    console.log(position);
-    const latitude =position.coords.latitude;
-    const longitude =position.coords.longitude;
-    const coordsObj={
-        latitude,
-        longitude
+function getWeather(lat, lng) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`)
+    //데이터를 가져올 때 사용함
+    //fetch(`https://~~~~~~~`);
+    .then(function (response) {
+    return response.json();
+    //ja에서 무언가가 끝나기를 기다리는 방법은 .then사용하는 것
+    })
+    .then(function name(json) {
+    const temperature = json.main.temp;
+    const place = json.name;
+    weather.innerText = `${temperature} @ ${place}`;
+    });
+}
+    
+function saveCoords(coordsObj) {
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+ }
+    
+function handleGeoSucces(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+    latitude,
+    longitude
     };
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
-
-function handleGeoError(){
-    console.log('cant access geo location');
-
+    
+function handleGeoError (params) {
+    console.log("cant access geo location");
 }
-
-function askForCoords(){
-    navigator.geolocation.getCurrentPosition(handleGeoSucces,handleGeoError);
-}
-
-function loadCoords(){
-
-    const loadedCords = localStorage.getItem(COORDS);
-    if(loadedCords===null){
-        askForCoords();
-    }else{
-        //getWeather
+    
+function askForCoords(params) {
+    navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError)
+}//내 위치 읽는거
+    
+function loadCoords(params) {
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords == null){
+    askForCoords();
+    } else{
+    const parsedCoords = JSON.parse(loadedCoords);
+    getWeather(parsedCoords.latitude, parsedCoords.longitude);
     }
-
 }
-
-function init (){
+    
+function init(params) {
     loadCoords();
-}
-
-init();
-
-
+ }
+    init();
